c1ec47c122fa5a79491ed1ce058515eb6ae0ff7e
Pasconsa <saulopascon@hotmail.com>
2022-06-30 16:44:35 -0300

    C15 Ruim solução com strings , Projeto Reserva Hotel , segunda tentativa de exceção. Objetivos das exceção Que as datas checkin não sejam depois da data checkout Que as datas de reservas sejam futuras.
    Versão Bad solution

--- Cap 15 Tratamento de excecoes/Exp02 Excecao Personalizada Reserva Hotel/src/application/Program.java
+++ Cap 15 Tratamento de excecoes/Exp02 Excecao Personalizada Reserva Hotel/src/application/Program.java
@@ -41,19 +41,13 @@ public class Program {
             System.out.print("Check-out date (dd/MM/yyyy): ");
             checkout = sdf.parse(sc.next());
 
- //Pessima solução para excecao. PELO FATO QUE ESSA SOLUÇÃO LOGICA DEVIA ESTA NA RESERVA E NÃO NO PROGRAMA PRINCIPAL
-            Date now = new Date();
-            if (checkin.before(now) || checkout.before(now)){
-                System.out.println("Error in reservation: Reservation dates for update must be future dates.");
-            }
-            else if(!checkout.after(checkin)) {
-                System.out.println("Error in reservation : Check-out date must be after check-in date");
-            }
-            else{
-                reservation.updateDates(checkin,checkout);
-                System.out.println("Reservation: " + reservation);
-            }
-
+           String error = reservation.updateDates(checkin, checkout);
+           if(error != null) {
+               System.out.println("Error in reservation: " + error);
+           }
+           else {
+               System.out.println("Reservation: " + reservation);
+           }
         }
 
     }
--- Cap 15 Tratamento de excecoes/Exp02 Excecao Personalizada Reserva Hotel/src/model/entities/Reservation.java
+++ Cap 15 Tratamento de excecoes/Exp02 Excecao Personalizada Reserva Hotel/src/model/entities/Reservation.java
@@ -11,10 +11,10 @@ public class Reservation {
     private Date checkout;
 
     //formatar a data para esse formato dd/mm/aaaa
-        //static para que outros objetos não seja instanciado
+    //static para que outros objetos não seja instanciado
     private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 
-    public Reservation(){
+    public Reservation() {
     }
 
     public Reservation(int roomNumber, Date checkin, Date checkout) {
@@ -41,29 +41,48 @@ public class Reservation {
     }
 
 
-    public long duration(){
+    public long duration() {
         long diff = checkout.getTime() - checkin.getTime();  // pega a diferença das duas datas em mili segundos.
-        return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS );  // converter mili segundos em dias
+        return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);  // converter mili segundos em dias
 
     }
 
-    public void updateDates (Date checkin, Date checkout){
+    public String updateDates(Date checkin, Date checkout) {
+
+
+        /* Solução 2 (ruim): método retornando string
+• A semântica da operação é prejudicada
+• Retornar string não tem nada a ver com atualização de reserva
+• E se a operação tivesse que retornar um string?
+• Ainda não é possível tratar exceções em construtores
+• Ainda não há auxílio do compilador: o programador deve "lembrar" de verificar se houve erro
+• A lógica fica estruturada em condicionais aninhadas*/
+
+        Date now = new Date();
+        if (checkin.before(now) || checkout.before(now)) {
+            return "Error in reservation: Reservation dates for update must be future dates.";
+        }
+        if (!checkout.after(checkin)) {
+            return "Error in reservation : Check-out date must be after check-in date";
+        }
         this.checkin = checkin;
         this.checkout = checkout;
+        return null;
     }
+
 
     //to string impressão
     @Override
-    public String toString(){
+    public String toString() {
         return "Room "
                 + number
                 + ", check-in: "
                 + sdf.format(checkin)
                 + ", check-out: "
                 + sdf.format(checkout)
-                +" "
+                + " "
                 + duration()
-                +" nights";
+                + " nights";
     }
 
 
